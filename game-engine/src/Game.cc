d3a9407370b52be61d8e1645726d7008287c1ebd
MikeLordx <miguelsantiagoews@gmail.com>
2021-09-22 09:49:13 -0500

    GameEngineEdit

--- game-engine/src/Game.cc
+++ game-engine/src/Game.cc
@@ -1,14 +1,12 @@
-#include "Game.hh"
-#include "Constants.hh"
-#include "Rectangle.hh"
-#include "InputSystem.hh"
-#include <iostream>
+#include "CommonHeaders.hh"
+#include "Player.hh"
 
 Rectangle *rectangle{new Rectangle(100, 100, 200, 100, sf::Color::Red)};
 sf::Clock *gameClock{new sf::Clock()};
 float deltaTime{};
-sf::Texture *texture1{new sf::Texture};
-sf::Sprite *sprite1{new sf::Sprite()};
+Player *player1{new Player("../assets/sprites.png", 4.f, 16, 16, 0, 5, 100, 25, 200.f)};
+Animation *idleAnimation{new Animation()};
+Animation *runAnimation{new Animation()};
 
 Game::Game()
 {
@@ -23,12 +21,8 @@ Game::~Game()
 //Starting things
 void Game::Start()
 {
-  texture1->loadFromFile("../assets/sprites.png");
-  sprite1->setTexture(*texture1);
-  sprite1->setTextureRect(sf::IntRect(0 * 16, 5 * 16, 16, 16));
-  sprite1->setPosition(100, 25);
-  sprite1->setColor(sf::Color::White);
-  sprite1->scale(4.f, 4.f);
+  idleAnimation = new Animation(player1->GetSprite(), 0, 5, 0.05f, 5);
+  runAnimation = new Animation(player1->GetSprite(), 0, 5, 0.08f, 6);
 }
 
 void Game::Initialize()
@@ -42,7 +36,15 @@ void Game::Update()
 {
   deltaTime = gameClock->getElapsedTime().asSeconds();
   gameClock->restart();
-  //std::cout << "elapsed time: " << deltaTime << std::endl;
+
+  if (std::abs(InputSystem::Axis().x) > 0)
+  {
+    runAnimation->Play(deltaTime);
+  }
+  else
+  {
+    idleAnimation->Play(deltaTime);
+  }
 }
 
 void Game::MainLoop()
@@ -73,14 +75,13 @@ void Game::Render()
 //Drawing sprites or geometry.
 void Game::Draw()
 {
-  window->draw(*rectangle->GetShape());
-  window->draw(*sprite1);
+  window->draw(*player1->GetSprite());
 }
 
 //Keyboard, joysticks, etc.
 void Game::Input()
 {
-  rectangle->GetShape()->move(sf::Vector2f(InputSystem::Axis().x * deltaTime * 1000.f, InputSystem::Axis().y * deltaTime * 1000.f));
+  player1->Move(deltaTime);
 }
 
 void Game::Destroy()
