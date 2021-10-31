aec88ad28e24e192ce0d4e9c18fee1176b36085a
Miguel Alejandro Santiago Perez <miguelsantiagoews@gmail.com>
2021-10-31 12:59:29 -0600

    Game engine updated

--- game-engine/src/Game.cc
+++ game-engine/src/Game.cc
@@ -1,14 +1,10 @@
 #include "CommonHeaders.hh"
 #include "Player.hh"
+#include "Animation.hh"
+#include "TileGroup.hh"
 
-//Rectangle* rectangle{new Rectangle(100, 100, 200, 100, sf::Color::Red)};
 
-//Physics Init
-b2Vec2* gravity{new b2Vec2(0.f, 0.f)};
-b2World* world{new b2World(*gravity)};
-b2Draw* drawPhysics{};
-
-std::vector<GameObject*>* Game::gameObjects{new std::vector<GameObject*>()};
+sf::CircleShape* circle{new sf::CircleShape()};
 
 TextObject* textObj1{new TextObject(ASSETS_FONT_ARCADECLASSIC, 14, sf::Color::White, sf::Text::Bold)};
 
@@ -16,8 +12,12 @@ sf::Clock* gameClock{new sf::Clock()};
 float deltaTime{};
 Player* player1{};
 GameObject* chest1{};
+GameObject* light1{};
 Animation* idleAnimation{new Animation()};
 Animation* runAnimation{new Animation()};
+
+TileGroup* tileGroup{};
+Tile* tile1{};
 
 uint32 flags{};
     //flags += b2Draw::e_aabbBit;
@@ -26,17 +26,33 @@ uint32 flags{};
     //flags += b2Draw::e_pairBit;
     //flags += b2Draw::e_jointBit;
 
+Animation* lightIdle{};
+
 Game::Game()
 {
   window = new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), GAME_NAME);
   event = new sf::Event();
+  gravity = new b2Vec2(0.f, 0.f);
+  world = new b2World(*gravity);
   drawPhysics = new DrawPhysics(window);
+  gameObjects = new std::vector<GameObject*>();
 
-  player1 = new Player(ASSETS_SPRITES, 4.f, 16, 16, 0, 5, 100, 25, 200.f,b2BodyType::b2_dynamicBody, world, window);
-  chest1 = new GameObject(ASSETS_SPRITES, 4.f, 16, 16, 6, 1, 300, 500,b2BodyType::b2_staticBody, world, window);
+  player1 = new Player(ASSETS_SPRITES, 4.f, 16, 16, 0, 5, 500, 300, 200.f, b2BodyType::b2_dynamicBody, world, window);
+  player1->SetTagName("Player");
+  chest1 = new GameObject(ASSETS_SPRITES, 4.f, 16, 16, 6, 1, 300, 500, b2BodyType::b2_staticBody, world, window);
+  chest1->SetTagName("chest");
+  light1 = new GameObject(ASSETS_SPRITES, 4.f, 16, 16, 6, 3, 500, 500, b2BodyType::b2_staticBody, world, window);
+  light1->SetTagName("light");
+  tileGroup = new TileGroup(window, 12, 12, ASSETS_MAPS, 4.f, 16, 16, ASSETS_TILES);
 
   AddGameObject(player1);
   AddGameObject(chest1);
+  AddGameObject(light1);
+
+  contactEventManager = new ContactEventManager(gameObjects);
+
+
+  lightIdle = new Animation(light1->GetSprite(), 6, 11, 0.1f, 3);
 }
 
 Game::~Game()
@@ -49,10 +65,15 @@ void Game::Start()
   flags += b2Draw::e_shapeBit;
   world->SetDebugDraw(drawPhysics);
   drawPhysics->SetFlags(flags);
+  world->SetContactListener(contactEventManager);
+
   textObj1->SetTextStr("Hello game engine");
   idleAnimation = new Animation(player1->GetSprite(), 0, 5, 0.05f, 5);
   runAnimation = new Animation(player1->GetSprite(), 0, 5, 0.08f, 6);
 
+  circle->setRadius(2.f);
+  circle->setFillColor(sf::Color::Green);
+  circle->setOutlineColor(sf::Color::Green);
 }
 
 void Game::Initialize()
@@ -78,6 +99,9 @@ void Game::Update()
     gameObject->Update(deltaTime);
   }
 
+  circle->setPosition(player1->GetSprite()->getPosition());
+
+  lightIdle->Play(deltaTime);
 
   if(std::abs(InputSystem::Axis().x) > 0 || std::abs(InputSystem::Axis().y) > 0)
   {
@@ -121,6 +145,9 @@ void Game::Draw()
 {
   //player1->Draw();
   //window->draw(*circle);
+
+  tileGroup->Draw();
+  //tile1->Draw();
 
   for(auto &gameObject : *gameObjects)
   {
